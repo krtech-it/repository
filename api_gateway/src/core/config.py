6de0851abba5b81325b9c7d257a8f55cb7c6019d
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-07-17 16:34:29 +0300

    api_gateway test

--- api_gateway/src/core/config.py
+++ api_gateway/src/core/config.py
@@ -8,12 +8,14 @@ load_dotenv()
 
 class Settings(BaseSettings):
     project_name: str = Field(..., env='PROJECT_NAME')
-    auth_url: str = Field(..., env='URL_AUTH')
-    auth_port: str = Field(..., env='PORT_AUTH')
+    auth_url: str = 'auth_api'
+    auth_port: str = '8010'
+    # auth_url: str = Field(..., env='URL_AUTH')
+    # auth_port: str = Field(..., env='PORT_AUTH')
 
-    # def __init__(self, **data):
-    #     super().__init__(**data)
-        # self.auth_url = f'http://{self.auth_url}:{self.auth_port}'
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.auth_url = f'http://{self.auth_url}:{self.auth_port}'
 
     class Config:
         env_file = '.env'
