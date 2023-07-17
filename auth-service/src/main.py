dc8e4326a1118a85c2f9ab513aa874707fa51085
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-07-17 10:14:22 +0300

    start_project

--- auth-service/src/main.py
+++ auth-service/src/main.py
@@ -12,6 +12,8 @@ from core.config import app_settings as settings
 from db import redis, postgres
 from db.postgres import create_database
 
+from api.v1 import auth
+
 
 app = FastAPI(
     title=settings.project_name,
@@ -20,19 +22,18 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
 )
 
+
 @app.on_event('startup')
 async def startup():
     redis.redis = Redis(host=settings.redis_host, port=settings.redis_port)
     from models.entity import User
     await create_database()
 
-# app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
-# app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
-# app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
+app.include_router(auth.router, prefix='/api/v1/login', tags=['login'])
 
-if __name__ == '__main__':
-    uvicorn.run(
-        'main:app',
-        host='0.0.0.0',
-        port=8000,
-    )
+# if __name__ == '__main__':
+#     uvicorn.run(
+#         'main:app',
+#         host='0.0.0.0',
+#         port=8000,
+#     )
