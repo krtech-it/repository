620dbbbf219634ae543e21b3ef5d6bb022146f7b
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-07-17 13:38:26 +0300

    api_gateway test

--- api_gateway/src/api/v1/gateway.py
+++ api_gateway/src/api/v1/gateway.py
@@ -9,6 +9,5 @@ router = APIRouter()
 @router.get('/login/')
 async def login():
     # x = {'auth': app_settings.url_auth}
-    print(app_settings.url_auth)
-    x = requests.get(f'{app_settings.url_auth}/api/v1/login/').json()
+    x = requests.get('0.0.0.0:8020/api/v1/login/').json()
     return x
--- api_gateway/src/core/config.py
+++ api_gateway/src/core/config.py
@@ -8,11 +8,6 @@ load_dotenv()
 
 class Settings(BaseSettings):
     project_name: str = Field(..., env='PROJECT_NAME')
-    url_auth: str = os.getenv('URL_AUTH')
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        self.url_auth = f'http://{self.url_auth}:8020'
 
     class Config:
         env_file = '.env'
