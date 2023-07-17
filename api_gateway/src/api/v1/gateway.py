2496ff40d11df7d3438db0540eacf76b7921cd15
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-07-17 15:43:02 +0300

    api_gateway test

--- api_gateway/src/api/v1/gateway.py
+++ api_gateway/src/api/v1/gateway.py
@@ -10,7 +10,7 @@ router = APIRouter()
 async def login():
     # x = {'auth': app_settings.url_auth}
     async with httpx.AsyncClient() as client:
-        response = await client.get('http://auth_api:8010/api/v1/login')
+        response = await client.get('http://176.124.198.110:8010/api/v1/login')
         data = response.status_code
         body = response.content
         print('-------------------------------')
