b4992cbbdeac91c4ef46ef31e377896f92925620
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-07-17 16:01:15 +0300

    api_gateway test

--- api_gateway/src/api/v1/gateway.py
+++ api_gateway/src/api/v1/gateway.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-import httpx
+import requests
 
 from core.config import app_settings
 
@@ -9,12 +9,11 @@ router = APIRouter()
 @router.get('/login/')
 async def login():
     # x = {'auth': app_settings.url_auth}
-    async with httpx.AsyncClient() as client:
-        response = await client.get('http://176.124.198.110:8010/api/v1/login')
-        data = response.status_code
-        body = response.content
-        print('-------------------------------')
-        print(data)
-        print(body)
-        print('-------------------------------')
-    return {1: 2}
+    response = requests.get('http://176.124.198.110:8010/api/v1/login')
+    data = response.status_code
+    body = response.content
+    print('-------------------------------')
+    print(data)
+    print(body)
+    print('-------------------------------')
+    return body
