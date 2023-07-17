b4f319e53d1d03e0e6787a7fdde142205a5f731f
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-07-17 11:46:19 +0300

    api_gateway test

--- api_gateway/src/api/v1/gateway.py
+++ api_gateway/src/api/v1/gateway.py
@@ -8,6 +8,6 @@ router = APIRouter()
 
 @router.get('/login/')
 async def login():
-    x = {'auth': app_settings.url_auth}
-    # x = requests.get(app_settings.url_auth).json()
+    # x = {'auth': app_settings.url_auth}
+    x = requests.get(app_settings.url_auth).json()
     return x
