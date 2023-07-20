159a5ff0dc2251436ee8716ee120c12d1a45bfb1
vladimir <vladimir1012kov@gmail.com>
2023-07-20 23:05:48 +0300

    Добавил класс CacheRedis для хранения невалидных токенов

---
+++ auth-service/src/services/redis_cache.py
@@ -0,0 +1,13 @@
+from redis.asyncio import Redis
+
+
+class CacheRedis:
+    def __init__(self, redis: Redis):
+        self.redis = redis
+
+    async def _object_from_cache(self, obj: str) -> bool:
+        data = await self.redis.get(obj)
+        return True if data else False
+    
+    async def _put_object_to_cache(self, obj: str, time_cache: int = 30):
+        await self.redis.set(obj, obj, time_cache)
