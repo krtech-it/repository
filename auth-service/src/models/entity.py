7a27572318573f91e0d5dfa9cd8a2f8204669134
jenny <eveydemann@gmail.com>
2023-07-20 20:05:44 +0300

    add models Role, History

--- auth-service/src/models/entity.py
+++ auth-service/src/models/entity.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
-
-from sqlalchemy import Boolean, Column, DateTime, String
+import enum
+from sqlalchemy import Boolean, Column, DateTime, String, Enum
 from sqlalchemy.dialects.postgresql import UUID
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -29,3 +29,52 @@ class User(Base):
 
     def __repr__(self) -> str:
         return f'<User {self.login}>'
+
+
+class RoleEnum(enum.Enum):
+    adult = "adult"
+    child = "child"
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_name = Column(String(255), unique=True, nullable=False)
+    subscription = Column(Boolean, nullable=False)
+    type = Column(Enum(RoleEnum))
+
+    def __init__(self, user_name: str, subscription: bool, type: enum) -> None:
+        self.user_name = user_name
+        self.subscription = subscription
+        self.type = type
+
+    def __repr__(self) -> str:
+        return f'<User {self.user_name}>'
+
+
+class EventEnum(enum.Enum):
+    login = "login"
+    logout = "logout"
+    refresh = "refresh"
+
+
+class History(Base):
+    __tablename__ = 'login_history'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_name = Column(String(255), unique=True, nullable=False)
+    time = Column(DateTime, default=datetime.utcnow)
+    browser = Column(String(255), nullable=False)
+    event_type = Column(Enum(EventEnum))
+    result = Column(Boolean, nullable=False)
+
+    def __init__(self, user_name: str, time: datetime, browser: str, event_type: enum, result: bool) -> None:
+        self.user_name = user_name
+        self.time = time
+        self.browser = browser
+        self.event_type = event_type
+        self.result = result
+
+    def __repr__(self) -> str:
+        return f'<User {self.user_name}>'
