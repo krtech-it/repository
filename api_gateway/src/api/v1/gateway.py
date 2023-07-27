ce69ed8485f05f9a0a1662b01a742056288ff85b
kirill petuhov <kirill.leto.kirov43@yandex.ru>
2023-07-27 11:42:27 +0300

    refresh method in api gateway

--- api_gateway/src/api/v1/gateway.py
+++ api_gateway/src/api/v1/gateway.py
@@ -25,7 +25,7 @@ async def login(response: Response, data: UserLogin, user_agent: Annotated[str |
 
 
 @router.post('/sign_up/')
-async def login(data: UserCreate):
+async def sign_up(data: UserCreate):
     url = f'{app_settings.auth_url}/api/v1/auth/sign_up/'
     response_auth = requests.post(url=url, json=data.dict())
     body = response_auth.json()
@@ -33,12 +33,25 @@ async def login(data: UserCreate):
 
 
 @router.get('/get_user/')
-async def login(request: Request, user_agent: Annotated[str | None, Header()] = None):
+async def get_user(request: Request, user_agent: Annotated[str | None, Header()] = None):
     url = f'{app_settings.auth_url}/api/v1/auth/get_user/'
     headers = {
         'User-Agent': user_agent,
     }
 
     response_auth = requests.get(url=url, headers=headers, cookies=request.cookies)
+    body = response_auth.json()
+    return body
+
+
+@router.post('/refresh/')
+async def refresh(request: Request, response: Response, user_agent: Annotated[str | None, Header()] = None):
+    url = f'{app_settings.auth_url}/api/v1/auth/refresh/'
+    headers = {
+        'User-Agent': user_agent,
+    }
+    response_auth = requests.post(url=url, headers=headers, cookies=request.cookies)
+    for cookie in response_auth.cookies.items():
+        response.set_cookie(key=cookie[0], value=cookie[1])
     body = response_auth.json()
     return body
