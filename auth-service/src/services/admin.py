12aee745720380e51a948c4b97be11afe5a17e3a
jenny <jenny1515@mail.ru>
2023-08-08 22:03:21 +0300

    add admin and change_role

--- auth-service/src/services/admin.py
+++ auth-service/src/services/admin.py
@@ -1,10 +1,58 @@
+import uuid
+
+from schemas.entity import UserCreate, UserLogin, UserProfil, ChangeProfil, ChangePassword, RoleCreate
+from models.entity import User, Role, EventEnum
+from services.repository import BaseRepository
+from services.auth_jwt import BaseAuthJWT
+from services.history import BaseHistory
+from services.redis_cache import CacheRedis
+from services.role import BaseRole
+from core.config import app_settings, ErrorName
 
 
-class BaseAdmin:
+class BaseAdmin(BaseRepository):
     '''
     Класс для методов администратора
     '''
 
-    def __init__(self, manager_auth, manager_role):
+    def __init__(self, manager_auth, manager_role, *args, **kwargs):
         self.manager_auth = manager_auth
         self.manager_role = manager_role
+        super().__init__(*args, **kwargs)
+
+    async def create_role(self, data: RoleCreate):
+        role = await self.manager_auth.get_obj_by_attr_name(Role, 'name_role', data.name_role)
+        if role:
+            return ErrorName.RoleAlreadyExists
+        new_role = {
+            "lvl": data.lvl,
+            "name_role": data.name_role,
+            "description": data.description,
+            "max_year": data.max_year
+        }
+        await self.create_obj(Role, new_role)
+        return new_role
+
+    async def update_role(self, role_id: uuid.UUID, new_data: RoleCreate):
+        role_obj: Role = await self.manager_role.get_role(role_id)
+        dict_data = dict(new_data)
+        if role_obj:
+            for attr, value in dict_data.items():
+                if value:
+                    setattr(role_obj, attr, value)
+            await self.manager_auth.session.commit()
+            return RoleCreate(**vars(role_obj))
+        else:
+            return ErrorName.RoleDoesNotExist
+
+    async def assign_role(self, role_id: uuid.UUID, user_id: uuid.UUID):
+        user_obj: User = await self.manager_auth.get_obj_by_pk(User, user_id)
+        role_obj: Role = await self.manager_auth.get_obj_by_pk(Role, role_id)
+        if role_obj:
+            user_obj.role_id = role_id
+            await self.manager_auth.session.commit()
+        else:
+            return ErrorName.RoleDoesNotExist
+
+    async def delete_role(self, role_id: uuid.UUID):
+        await self.delete_obj(Role, role_id)
