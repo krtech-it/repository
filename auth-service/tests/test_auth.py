e75d4fe4bcfcfdf66327d0642346129660e329d0
Кирилл Петухов <test@mail.ru>
2023-08-09 22:38:36 +0300

    test sign_up

--- auth-service/tests/test_auth.py
+++ auth-service/tests/test_auth.py
@@ -8,6 +8,7 @@ from uuid import uuid4
 
 START_URL = "/api/v1/auth/"
 
+
 @pytest.mark.parametrize(
     'query_data, expected_answer, mock_get_user',
     [
@@ -53,6 +54,30 @@ async def test_login(query_data, expected_answer, mock_get_user, ac: AsyncClient
                 {'status': HTTPStatus.OK, 'response_body': None},
                 tuple(),
                 [None, Role(lvl=0, name_role="str", description="str", max_year="int")]
+        ),
+        (
+                {"login": "admin", "password": "stringst", "first_name": "string", "last_name": "string", "email": "test@test.ru"},
+                {'status': HTTPStatus.OK, 'response_body': None},
+                tuple(),
+                [Role(lvl=0, name_role="str", description="str", max_year="int")]
+        ),
+        (
+                {"login": "admin", "password": "stringst", "first_name": "string", "last_name": "string",
+                 "email": "test@test.ru"},
+                {'status': HTTPStatus.BAD_REQUEST,
+                 'response_body': {"detail": "Пользователь с таким login уже существует"}},
+                 (User(login='admin', password='admin', first_name='dima', last_name='ivanov', role_id=uuid4(),
+                       email='test@mail.ru', is_admin=False),),
+                [Role(lvl=0, name_role="str", description="str", max_year="int")]
+        ),
+        (
+                {"login": "admin", "password": "stringst", "first_name": "string", "last_name": "string",
+                 "email": "test@test.ru"},
+                {'status': HTTPStatus.BAD_REQUEST,
+                 'response_body': {"detail": "Пользователь с таким email уже существует"}},
+                 (User(login='user', password='admin', first_name='dima', last_name='ivanov', role_id=uuid4(),
+                       email='test@test.ru', is_admin=False),),
+                [Role(lvl=0, name_role="str", description="str", max_year="int")]
         )
         # (
         #         {'login': 'admin', 'password': 'admin'},
@@ -73,13 +98,16 @@ async def test_sign_up(query_data, expected_answer, mock_get_users, mock_get_rol
 
     async def mock_return_null(*args, **kwargs):
         return None
+
     async def mock_get_first_obj_order_by_attr_name(*args, **kwargs):
         nonlocal x
         if x == 0:
             x+=1
             return mock_get_role[0]
         else:
-            return
+            role = mock_get_role[1]
+            role.id = uuid4()
+            return role
 
     monkeypatch.setattr('services.user.BaseRepository.get_list_obj_by_list_attr_name_operator_or',
                         mock_get_list_obj_by_list_attr_name_operator_or)
